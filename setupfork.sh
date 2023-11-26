83bbb89f41653953e5bf76bd7734d50505a50dbc
mcfrank <mcfrank@goldenticket.local>
2023-11-26 11:15:10 +0100

    added setupfork.sh

---
+++ setupfork.sh
@@ -0,0 +1,6 @@
+# Step 1: Fork and clone the neuralmesh/apimesh repository using GitHub CLI
+gh repo fork neuralmesh/apimesh --org neuralmesh --fork-name=microservergen --clone --remote
+gh repo clone neuralmesh/microservergen
+cd microservergen
+gh repo set-default neuralmesh/microservergen
+
