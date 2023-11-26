7dc00af482b0d3b98717c7de18c41acb9a610327
mcfrank <mcfrank@goldenticket.local>
2023-11-26 18:56:10 +0100

    using env var as openai token

--- apimesh.py
+++ apimesh.py
@@ -3,7 +3,7 @@ from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 
 def process_issue_data(issue_data, openai_api_key):
-    llm = LLMChain(llm=OpenAI(api_key=openai_api_key))
+    llm = LLMChain(llm=OpenAI())
 
     response = llm.ainvoke({"topic": issue_data})
     return response.content
