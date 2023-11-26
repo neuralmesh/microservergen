32140f5708e1d9163c42c123eed13b31bd567987
mcfrank <mcfrank@goldenticket.local>
2023-11-26 19:08:00 +0100

    refactored apimesh.py

--- apimesh.py
+++ apimesh.py
@@ -1,18 +1,26 @@
+import os
 import sys
-from langchain.chains import LLMChain
 from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
 
-def process_issue_data(issue_data):
-    llm = LLMChain(llm=OpenAI())
+# Read issue data from the command line argument
+issue_data = sys.argv[1]
 
-    response = llm.ainvoke({"topic": issue_data})
-    return response.content
+# Get the OpenAI API key from environment variables
+api_key = os.environ['OPENAI_API_KEY']
 
-if __name__ == "__main__":
-    issue_data = sys.argv[1]
-    
-    response_content = process_issue_data(issue_data)
-    
-    with open("ai_response.txt", "w") as file:
-        file.write(response_content)
+# Initialize LangChain with OpenAI
+llm = OpenAI(api_key=api_key, temperature=0.7)  # Adjust temperature as needed
+prompt_template = "Given the following GitHub issue details: {issue_details}, what would be a good solution? Be concise, neutral and professional. aim for the asymptotically highest information density/text ratio"
+
+# Setup LLMChain
+llm_chain = LLMChain(llm=llm, prompt=PromptTemplate.from_template(prompt_template))
+
+# Process issue data
+response = llm_chain.predict(issue_details=issue_data)
+
+# Save the response to a file
+with open('ai_response.txt', 'w') as file:
+    file.write(response)
 
